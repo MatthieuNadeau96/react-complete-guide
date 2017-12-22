34166f8955825d2d819b49fc9204e532fb23f775
Matthieu <matthieunadeau96@gmail.com>
2017-12-22 12:55:12 -0500

    Pseudo Selectors & Media Queries

--- src/App.js
+++ src/App.js
@@ -43,17 +43,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      transition: '400ms'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -68,8 +59,7 @@ class App extends Component {
           })}
         </div>
       );
-
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
     const assignedClasses = ['transition'];
@@ -88,7 +78,7 @@ class App extends Component {
           <h1>Hi, I'm a React App</h1>
           <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button
-            style={style}
+            className={btnClass}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
             {persons}
         </div>
