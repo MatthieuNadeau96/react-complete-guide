3a9ba4794be232ceb1696be8546c9765fcf112b1
Matthieu <matthieunadeau96@gmail.com>
2017-12-24 20:31:09 -0500

    Performance Gains with PureComponents

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log("[App.js] Inside Constructor", props);
@@ -26,11 +26,11 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
-    // return nextProps.persons !== this.props.persons;
-    return true;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //   return nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons;Pure
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
@@ -93,6 +93,7 @@ class App extends Component {
 
     return (
         <div className={classes.App}>
+          <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
           <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
