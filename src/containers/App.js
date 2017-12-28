8651918cb5bd167fba3ee462f0349ba9aa0df74a
Matthieu <matthieunadeau96@gmail.com>
2017-12-28 11:52:27 -0500

    Using setStateCorrectly

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,8 +16,9 @@ class App extends PureComponent {
         { id: '9gkji', name: 'Cameron', age: 19}
       ],
       otherState: 'some other value',
-      showPersons: false
-    }
+      showPersons: false,
+      toggleClicked: 0
+    };
   }
 
   componentWillMount() {
@@ -79,7 +80,13 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState( (prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+
+    });
   }
 
   render() {
